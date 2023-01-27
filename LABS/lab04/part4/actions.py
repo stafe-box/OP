83260ae025bc2a1800e91f90de0d7bda39ffc07a
stafe-box <stafe_box@outlook.com>
2023-01-27 20:09:39 +0500

    lab 4 part 4 is interactive now

--- LABS/lab04/part4/actions.py
+++ LABS/lab04/part4/actions.py
@@ -8,25 +8,52 @@ engine = create_engine('sqlite:///sqlite3.db')
 session = Session(bind=engine)
 
 
-def add_kind_control(kind):
+def add_kind_control():
+    kind = input("Введите название вида контроля>>> ")
+    kinds = session.query(models.KindControl).filter(models.KindControl.title.is_(kind))
+    if kinds.count() > 0:
+        warn("Такой вид контроля уже существует")
+        return
     new_kind = models.KindControl(title=kind)
     session.add(new_kind)
     session.commit()
+    sucsess("Вид контроля успешно добавлен")
 
 
-def add_teacher(full_name):
+def add_teacher():
+    full_name = input("Введите ФИО препрдователя>>> ")
+    teachers = session.query(models.Teacher).filter(models.Teacher.full_name.is_(full_name))
+    if teachers.count() > 0:
+        warn("Такой препрдователь уже существует")
+        return
     new_teacher = models.Teacher(full_name=full_name)
     session.add(new_teacher)
     session.commit()
+    sucsess("Преподователь успешно добавлен")
 
 
-def add_discipline(id_kind, discipline_name, lectures, practices, laboratory):
-    kinds = session.query(models.KindControl).filter(models.KindControl.id == id_kind)
-    assert kinds.count(), f"id_kind <{id_kind}> not found"
+def add_discipline():
+    name = input('Введите название дисиплины>>> ')
+    disciplines = session.query(models.Discipline).filter(
+        models.Discipline.discipline_name.is_(name))
+    if disciplines.count() > 0:
+        warn("Такая дисциплина уже существует")
+        return
+    kinds = session.query(models.KindControl)
+    print ("Выберите вид контроля:")
+    for kind in kinds:
+        print (f"\t{kind.id} - {kind.title}")
+    id_kind = int(input(">>> "))
+    if (id_kind > kinds.count()) or (id_kind < 1):
+        warn(f"Вид контроля с номером {id_kind} не найден")
+        return
     kind = kinds.first()
+    lectures = int(input("Введите количество лекций>>> "))
+    practices = int(input("Введите количество практик>>> "))
+    laboratory = int(input("Введите количество лабораторных>>> "))
 
     new_discipline = models.Discipline(
-        discipline_name=discipline_name,
+        discipline_name=name,
         lectures=lectures,
         practices=practices,
         laboratory=laboratory,
@@ -37,14 +64,32 @@ def add_discipline(id_kind, discipline_name, lectures, practices, laboratory):
 
     session.add(new_discipline)
     session.commit()
+    sucsess("Дисциплина успешно добавлена")
 
 
-def append_teachers_to_disciplines(id_discipline, id_teachers):
+def append_teachers_to_disciplines():
+    disciplines = session.query(models.Discipline)
+    print ("Выберите дисциплину")
+    for discipline in disciplines:
+        print (f"\t{discipline.id} - {discipline.discipline_name}")
+    id_discipline = int(input(">>> "))
+    # k = disciplines.count()
+    # print (k)
+    nDiscipline = session.query(models.Discipline).filter(models.Discipline.id.is_(id_discipline))
+    if not(nDiscipline.exists):
+        warn(f"Дисциплина с номером{id_discipline} не найдена")
+        return
+    all_t = session.query(models.Teacher)
+    print("Выберите преподователей")
+    for teacher in all_t:
+        print(f"\t{teacher.id} - {teacher.full_name}")
+    Snums = input("Введите номера через запятую\n>>>")
+    id_teachers = Snums.split(", ")
     teachers = session.query(models.Teacher).filter(models.Teacher.id.in_(id_teachers))
-    assert teachers.count(), f"id_feeds {id_teachers} not found"
+    if teachers.count() < 1:
+        warn(f"Преподователеи с номерами {id_teachers} не найдены")
 
     disciplines = session.query(models.Discipline).filter(models.Discipline.id == id_discipline)
-    assert disciplines.count(), f"id_animal <{id_discipline}> not found"
     discipline = disciplines.first()
 
     for teacher in teachers.all():
@@ -54,20 +99,22 @@ def append_teachers_to_disciplines(id_discipline, id_teachers):
 
     session.add(discipline)
     session.commit()
+    sucsess("Преподователи успешно связанны с дисциплинами")
 
 
 def all_disciplines():
     disciplines = session.query(models.Discipline).all()
     print("Все дисциплины:")
     for discipline in disciplines:
-        print(f"\t<{discipline.discipline_name}>")
+        print(f"\t{discipline.discipline_name}")
 
-        print("\tПреподаватели:")
+        print("\t|__Преподаватели:")
         for teacher in discipline.teachers:
-            print(f"\t\t{teacher.full_name}")
+            print(f"\t|\t|__{teacher.full_name}")
 
         kind = session.query(models.KindControl).filter(models.KindControl.id == discipline.kind_control_id)
-        print(f"\tВид контроля: {kind.first().title}\n")
+        print(f"\t|__Вид контроля: \n\t\t|__{kind.first().title}\n")
+    input("\033[94mНажмите ВВОД чтобы продолжить\033[0m")
 
 
 def all_teachers():
@@ -76,15 +123,29 @@ def all_teachers():
     print("Все преподаватели:")
     for teacher in teachers:
         print(f"\tФИО: {teacher.full_name}, число дисциплин: {len(teacher.disciplines)}")
+    input("\033[94mНажмите ВВОД чтобы продолжить\033[0m")
 
 
-def one_teacher(name):
+def one_teacher():
+    name = input("Введите ФИО преподователя >>> ")
     teachers = session.query(models.Teacher).filter(
         models.Teacher.full_name == name)
-    assert teachers.count(), f"name <{name}> not found"
-
+    if teachers.count() < 1:
+        warn("Такого преподователя не сушествует")
+        return
     teacher = teachers.first()
     inf = (f"ФИО: {teacher.full_name}, дициплины: ")
     for discipline in teacher.disciplines:
         inf += f"\n\t{discipline.discipline_name}"
     print(inf)
+    input("\033[94mНажмите ВВОД чтобы продолжить\033[0m")
+
+
+def warn(text):
+    print(f"\033[93m{text}\033[0m")
+
+def sucsess(text):
+    print(f"\033[92m{text}\033[0m")
+
+def fatal(text):
+    print(f"\033[91m{text}\033[0m")
--- LABS/lab04/part4/main.py
+++ LABS/lab04/part4/main.py
@@ -1,15 +1,34 @@
 import actions
 
-# Вывод информации
-print(
-    "Choose the option:" +
-    "\n\t1 - Show all teachers"+
-    "\n\t2 - Show all disciplines"+
-    "\n\t3 - Show one teacher"+
-    "\n\t4 - Add teacher"+
-    "\n\t5 - Add discipline"+
-    "\n\t6 - Connect discipline to teacher"+
-    "\n\t7 - Connect discipline to controll\n")
-#TODO: сделать коннект по именам, запилить логику интерфейса
-actions.all_disciplines()
-actions.all_teachers()
+while True:
+    print(
+        "\nВыберите действие:" +
+        "\n\t1 - Показать всех преподавателей" +
+        "\n\t2 - Показать все дисциплны" +
+        "\n\t3 - Показать одного препрдавателя" +
+        "\n\t4 - Добавить преподавателя" +
+        "\n\t5 - Добавить дисциплину" +
+        "\n\t6 - Добавить вид контроля" +
+        "\n\t7 - Связать дисциплину и преподавателей" +
+        "\n\t8 - Выйти\n")
+    choose = int(input(">>> "))
+    match choose:
+        case 1:
+            actions.all_teachers()
+        case 2:
+            actions.all_disciplines()
+        case 3:
+            actions.one_teacher()
+        case 4:
+            actions.add_teacher()
+        case 5:
+            actions.add_discipline()
+        case 6:
+            actions.add_kind_control()
+        case 7:
+            actions.append_teachers_to_disciplines()
+        case 8:
+            exit(0)
+        case _:
+            actions.warn("Такого варианта действия нет")
+
